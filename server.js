20adddaafa6b3ddd546e6040b5aa8e5ea5bac211
dxke <lennart.herzog@icloud.com>
2024-06-30 18:56:02 +0200

    additional formatting + minor changes in server.js

--- server.js
+++ server.js
@@ -7,32 +7,32 @@ const path = require("path");
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
   if (req.method === "POST" && req.url === "/check-url") {
-    // Set CORS headers
-    res.setHeader("Access-Control-Allow-Origin", "*"); // security risk in production
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-    // Handle preflight requests
-    if (req.method === "OPTIONS") {
-      res.writeHead(204);
-      res.end();
-      return;
-    }
-
-    // get the URL from the request body
+    // read the body of the request
     let body = "";
     req.on("data", (chunk) => {
       body += chunk.toString();
     });
 
-    // parse the URL and construct a file path
+    // parse the URL and construct the pathname
     req.on("end", () => {
       const { url: inputUrl } = JSON.parse(body);
       const parsedUrl = new URL(inputUrl);
       const pathname = path.join(__dirname, parsedUrl.pathname);
 
-      // error handling for invalid URL
+      // first: error handling
       fs.stat(pathname, (err, stats) => {
         if (err) {
           res.writeHead(200, { "Content-Type": "application/json" });
@@ -45,7 +45,7 @@ const server = http.createServer((req, res) => {
         }
 
         let message;
-        //check if file or directory exists or not
+        // check if URL points to a file or folder
         if (stats.isFile()) {
           message = `File exists: ${pathname}`;
         } else if (stats.isDirectory()) {
@@ -53,7 +53,7 @@ const server = http.createServer((req, res) => {
         } else {
           message = "URL does not point to a file or folder that exists.";
         }
-        // send the response
+        // send response
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message }));
       });
@@ -64,7 +64,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-//make the server listen for requests
+// listen for requests
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
