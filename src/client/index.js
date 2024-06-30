935fa7ec20e121d423dc012d819fc321003af402
dxke <lennart.herzog@icloud.com>
2024-06-30 17:40:07 +0200

    add html elements to index.js

---
+++ src/client/index.js
@@ -0,0 +1,24 @@
+// import the stylesheet
+import "./style.css";
+
+// add the html elements
+function component() {
+  const element = document.createElement("div");
+
+  // Create input element
+  const inputURL = document.createElement("input");
+  element.appendChild(inputURL);
+
+  // Create paragraph element
+  const pResult = document.createElement("p");
+  pResult.textContent = "Please enter a valid URL.";
+  element.appendChild(pResult);
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+// get the input and paragraph elements for reference
+const inputURL = document.querySelector("input");
+const pResult = document.querySelector("p");
